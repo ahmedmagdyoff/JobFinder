a886a36279d796da4804fe83e7fcbb686e6779ad
AHMED MAGDY <ahmedmagdy30003@gmail.com>
2025-12-15 22:21:44 +0200

    a

--- footer.php
+++ footer.php
@@ -36,6 +36,7 @@
 <script src="assets/js/script.js"></script>
 <script src="assets/js/login.js"></script>
 <script src="assets/js/jobs.js"></script>
+<script src="assets/js/job.js"></script>
 <script src="assets/js/contact.js"></script>
 
 </body>
--- header.php
+++ header.php
@@ -10,6 +10,7 @@
     <link href="assets/css/login.css" rel="stylesheet" />
     <link href="assets/css/blog.css" rel="stylesheet" />
     <link href="assets/css/jobs.css" rel="stylesheet" />
+    <link href="assets/css/job.css" rel="stylesheet" />
     <link href="assets/css/companies.css" rel="stylesheet" />
     <link href="assets/css/contact.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet" />
